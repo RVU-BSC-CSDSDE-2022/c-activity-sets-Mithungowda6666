8e2736e6699335bbe501e67b26918e8ae3aefd0d
Mithun Gowda  Gowda <>
2022-10-17 05:30:44 +0000

    problem03 solution

---
+++ set01/problem03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int sum_fun(int a, int b);
+
+
+int main(void) {
+  int a, b,sum;
+  printf("enter the first number");
+  scanf("%d",&a);
+  printf("enter the second number");
+  scanf("%d",&b);
+  sum=sum_func(a,b);
+  printf("%d", sum);
+
+return 0;
+  }
+int sum_func(int a, int b)
+{int sum;
+  sum=a+b;
+  return sum;
+  }
