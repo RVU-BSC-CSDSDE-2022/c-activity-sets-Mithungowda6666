aa7b571f18eb0ef15d70fa1fba4f1379e56212a6
Mithun Gowda  Gowda <115687893+Mithungowda6666@users.noreply.github.com>
2022-10-31 07:42:01 +0000

    solution 8

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+ int input_arry_size();
+void input_arry(int n, int a[n]);
+int sum_n_arry(int n, int a[n]);
+void output(int n, int a[n])
+
+int main(){
+  int n, sum;
+  n=input_arry_size();
+  int a[n];
+  input_arry(n);
+  
+}
---
+++ set01/problem11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  x = 4
+if x>2 :
+  printf("bigger")
+else :
+  printf("smaller")
+  
+}
