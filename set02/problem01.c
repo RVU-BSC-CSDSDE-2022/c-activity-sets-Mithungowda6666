b6a89b849f9d84ef8653d798c992ee90c43e180d
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-07 03:08:54 +0000

    problem01 solved

--- set02/problem01.c
+++ set02/problem01.c
@@ -4,7 +4,6 @@ struct _point {
   float x;
   float y;
 };
-
 typedef struct _point Point;
 
 Point input();
@@ -12,7 +11,7 @@ void dist(Point a, Point b, float *res);
 void output(Point a, Point b, float res);
 
 int main() {
-  float a,b;
+  Point a,b;
   printf("enter the value of the x1 & y1\n");
   a = input();
   printf("enter the value of the x2 & y2\n");
@@ -27,8 +26,9 @@ Point input() {
   return(a);
 }
 void dist(Point a, Point b, float *res) {
-  *res = sqrt(pow((b.x-a.x),2)+pow((b.y-a.y),2);
+  *res = sqrt(pow((b.x-a.x),2)+pow((b.y-a.y),2));
  }
+
 void output( Point a, Point b, float res) {
   printf("the distance between (%2.1f,%2.1f) and (%2.1f,%2.1f) is %2.1f",a.x,b.x,a.y,b.y,res);
 }
