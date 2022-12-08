dae8a89304f8ee45068a6517ed4018feabeeac2f
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-08 15:12:32 +0000

    changes

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 void input_camel_details(float *radius, float *height, float *length);
 float find_weight(float radius, float height, float length);
 void output(float radius, float height, float length, float weight);
