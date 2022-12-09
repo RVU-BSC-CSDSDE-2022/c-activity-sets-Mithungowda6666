4589ad8e7dc7ccea096cfb95685eaad3cfcff533
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-09 09:44:12 +0000

    finally solved

--- set02/problem02.c
+++ set02/problem02.c
@@ -5,28 +5,31 @@ float find_weight(float radius, float height, float length);
 void output(float radius, float height, float length, float weight);
 
 int main() {
-  float radius, height, length;
-  input(radius, height, length);
-  find_weight(radius,height,length);
-    return 0;
+  float radius, height, length , weight;
+  input_camel_details(&radius, &height, &length);
+  weight = find_weight(radius,height,length);
+    output(radius,height,length,weight);
+    
 }
 void input_camel_details(float *radius, float *height, float *length){
-  float weight, pi;
+  float weight;
   printf("Enter the value of the Radius");
-  scanf("%f",&radius);
+  scanf("%f",radius);
   printf("Enter the value of the Height");
-  scanf("%f",&height);
+  scanf("%f",height);
   printf("Enter the value of Length");
-  scanf("%f",&length);  
-  find_weight(&radius,&height,&length,&weight);
-  output(radius,height,length,weight);
+  scanf("%f",length);  
+
 }
 float find_weight(float radius, float height, float length){
    float pi = 3.14;
-  weight = (pi * stomach_radius^3 * sqrt(height * length));
-   return 1;
+  float weight;
+  weight =pi*pow(radius,3)*sqrt(height * length);
+   return weight;
 
 }
 void output(float radius, float height, float length, float weight){
   printf("The weight of the camel with radius: %f height:%f, length:%f is %f",radius,height,length,weight);
 }
+// to compile the program usegcc problem02.c -lm -o problem02//
+//to get the output use ./problem02//
