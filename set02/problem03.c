37beb7b2097fe1e912949db8942ae76dfd30d992
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-12 12:29:58 +0000

    about to solve

--- set02/problem03.c
+++ set02/problem03.c
@@ -10,5 +10,19 @@ void find_weight(Camel *c); //passing address variable
 void output(Camel c);
 
 Camel input() {
-  printf("")
+  float a;
+  printf("Enter the value of Radius\n");
+  scanf("%f",&Camel.radius);
+  printf("Enter the value of Height\n");
+  scanf("%f",&Camel.height);
+  printf("Enter the value of Length\n");
+  scanf("%f",&Camel.length);
+  return (a);
+}
+void find_weight(Camel *c){
+  *c.weight = pi*pow(radius,3)*sqrt(height * length);
+}
+void output(Camel c) {
+  printf("The weight of the camel with radius: %f, height: %f, length: %f is %f",
+Write")
 }
