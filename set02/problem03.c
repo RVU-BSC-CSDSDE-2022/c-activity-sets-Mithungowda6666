4c77a4f7dd0ca792c66ca2d252cc9f9b16a5f0ad
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-13 04:45:27 +0000

    errors

--- set02/problem03.c
+++ set02/problem03.c
@@ -4,25 +4,34 @@ struct camel {
 };
 
 typedef struct camel Camel;
-
 Camel input();
 void find_weight(Camel *c); //passing address variable
 void output(Camel c);
 
+int main() {
+  Camel_radius = Camel input();
+  Camel_height = Camel input();
+  Camel_length = Camel input();
+   find_weight(Camel *c);
+  output(Camel c);
+    return 0;
+  
+}
+
 Camel input() {
-  float a;
+  float c;
   printf("Enter the value of Radius\n");
-  scanf("%f",&Camel.radius);
+  scanf("%f",&c.radius);
   printf("Enter the value of Height\n");
-  scanf("%f",&Camel.height);
+  scanf("%f",&c.height);
   printf("Enter the value of Length\n");
-  scanf("%f",&Camel.length);
-  return (a);
+  scanf("%f",&c.length);
+  return (c);
 }
 void find_weight(Camel *c){
-  *c.weight = pi*pow(radius,3)*sqrt(height * length);
+  
+  *c_weight = pi*pow(radius,3)*sqrt(height * length);
 }
 void output(Camel c) {
-  printf("The weight of the camel with radius: %f, height: %f, length: %f is %f",
-Write")
+  printf("The weight of the camel with radius: %f, height: %f, length: %f is %f",Camel.radius,Camel.lenght,Camel.height,*c.weight);
 }
