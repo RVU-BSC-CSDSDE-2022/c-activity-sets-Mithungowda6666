fdf0f1c88cd74155510c14f272cc8ef5d3a8fae0
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-14 07:03:01 +0000

    doing

---
+++ set02/problem04.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+void input_camel_details(float *radius, float *height, float *length);
+int find_mood(float radius, float height, float length)
+void output(float radius, float height, float length, int mood);
