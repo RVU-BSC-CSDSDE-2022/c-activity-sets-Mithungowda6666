81785346d60284325554e0c681bb6f0ec92791f1
Mithun Gowda  D.R <115687893+Mithungowda6666@users.noreply.github.com>
2022-12-15 12:30:43 +0000

    warnings removed

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
 void input_two__strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 
 int main(){
   char string1[100],string2[100];
-  input_two_strings(string1,string2);
+  input_two__strings(string1,string2);
   int r = 10;
   r = stringcompare(string1,string2);
   output(string1,string2,r);
 }
 
-void input_two_strings(char *string1, char *string){
-  print("Enter the first string ");
-  scanf("%s", string1);
+void input_two__strings(char *string1, char *string2){
+  printf("Enter the first string ");
+  if(scanf("%s", string1)!=1)
+  {
+    printf("error");
+    exit(0);
+  }
   printf("Enter the second string ");
-  scanf("%s", string2);
+  if(scanf("%s", string2)!=1)
+    {
+    printf("error");
+    exit(0);
+  }
   }
 
 int stringcompare(char *string1, char *string2){
